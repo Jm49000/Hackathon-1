fb670643d5860380c402e3667f7e8e754c0d5dfc
Thomas Lavo <thomaslavo@macbook-air-de-thomas.home>
2022-05-13 03:08:26 +0200

    Adding call API, routes, modal, some styles

--- src/assets/components/HomePage.jsx
+++ src/assets/components/HomePage.jsx
@@ -3,33 +3,54 @@ import "./button.css";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
+  const audio = new Audio("https://www.mboxdrive.com/hey.mp3");
+  const start = () => {
+    audio.play();
+  };
+  const stop = () => {
+    audio.pause();
+  };
   return (
-    <div className="solar-syst">
-      <div className="sun" />
-      <div className="mercury" />
-      <div className="venus" />
-      <div className="earth" />
-      <div className="mars" />
-      <div className="jupiter" />
-      <div className="saturn" />
-      <div className="uranus" />
-      <div className="neptune" />
-      <div className="asteroid-belt" />
-      <Link to="/wildearth" className="animated-button1">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        Entrez sur le site
-      </Link>
+    <section>
+      <button onClick={start} className="sound">
+        ▶️
+      </button>
+      <button onClick={stop} className="sound">
+        ⏸
+      </button>
+      <div class="bg-animation">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div id="stars4"></div>
+      </div>
+      <div className="solar-syst">
+        <div className="sun" />
+        <div className="mercury" />
+        <div className="venus" />
+        <div className="earth" />
+        <div className="mars" />
+        <div className="jupiter" />
+        <div className="saturn" />
+        <div className="uranus" />
+        <div className="neptune" />
+        <div className="asteroid-belt" />
+        <Link to="/wildearth" className="animated-button1">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Entrer sur Terre
+        </Link>
 
-      <Link to="/contact" className="buttonContact">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        Nous contacter
-      </Link>
-    </div>
+        <Link to="/contact" className="buttonContact">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Nous contacter
+        </Link>
+      </div>
+    </section>
   );
 }
--- src/assets/components/MainComponent.jsx
+++ src/assets/components/MainComponent.jsx
@@ -1,9 +1,25 @@
 import "../components/sass/MainComponent.scss";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function MainComponent() {
+  const [showModal, setShowModal] = useState(false);
   const [arrayIq, setArrayIq] = useState([]);
   const [filter, setFilter] = useState("");
+  const Modal = (props) => {
+    return (
+      <div className={`modal ${props.show ? "active" : ""}`}>
+        {props.children}
+      </div>
+    );
+  };
+  const ModalBody = (props) => {
+    return <div className="modal__body ">{props.children}</div>;
+  };
+  const ModalFooter = (props) => {
+    return <div className="modal__footer ">{props.children}</div>;
+  };
   function getArrayIq() {
     const air = [];
     axios
@@ -30,18 +46,59 @@ export default function MainComponent() {
   function handleKeyDown(e) {
     if (e.key === "Enter") {
       setFilter(e.target.value);
+      setShowModal(true);
     }
   }
   return (
     <section>
+      <Link to="/" className="btn-home">
+        Accueil
+      </Link>
       <div className="search">
         <input
           type="text"
           name="name"
-          placeholder="Tapez ici votre ville et appuiyez sur entrer"
+          placeholder="Tapez ici votre ville"
           onKeyPress={handleKeyDown}
         />
       </div>
+      <Modal show={showModal}>
+        <ModalBody>
+          <ul style={{ textAlign: "justify" }}>
+            {arrayIq.map((item) => (
+              <div>
+                <h2>{item.placeName}</h2>
+                <li key={item.city}></li>
+                <li>
+                  <img
+                    src="https://img.icons8.com/external-smashingstocks-circular-smashing-stocks/65/000000/external-wind-weather-smashingstocks-circular-smashing-stocks.png"
+                    className="center"
+                  />
+                  <br />
+                  Indice de Qualité de l'Air | {item.AQI}
+                </li>
+                <li>Monoxyde de Carbonne | {item.CO}</li>
+                <li>Couche d'Ozone | {item.OZONE}</li>
+                <li>
+                  Particules en suspension
+                  <br /> inférieur à 2,5 um | {item.PM10}
+                </li>
+                <li>
+                  Particules en suspension
+                  <br /> inférieur à 10 um | {item.PM25}
+                </li>
+                <li>Dioxyde de Sulfure | {item.SO2}</li>
+                <li>Dioxyde de Nitrogène | {item.NO2}</li>
+              </div>
+            ))}
+          </ul>
+        </ModalBody>
+        <ModalFooter>
+          <button onClick={() => setShowModal(false)} className="modal-btn">
+            Fermer
+          </button>
+        </ModalFooter>
+      </Modal>
       <div class="bg-animation">
         <div id="stars"></div>
         <div id="stars2"></div>
@@ -56,11 +113,6 @@ export default function MainComponent() {
           <div class="inner-shadow"></div>
         </div>
       </div>
-      <ul>
-        {arrayIq.map((item) => {
-          return <Modal key={item} />;
-        })}
-      </ul>
     </section>
   );
 }
